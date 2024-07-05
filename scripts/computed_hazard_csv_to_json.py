2c05a30735dfd6445def9d142e67d0bb6b32d340
Priydarshi Singh <dryairship@gmail.com>
2024-07-05 20:09:36 +0200

    Add python scripts to convert csv to json

---
+++ scripts/computed_hazard_csv_to_json.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv ("../public/Computed_Hazard.csv")
+# df['Filename'] = df['Filename'].apply(lambda x: x.lower() if isinstance(x, str) else x)
+df = df.set_index('Filename')
+df.to_json("../public/Computed_Hazard.json", index='Filename', orient='index')
