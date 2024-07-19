5daa3e3adbc81a74bdee8fb9ca56db46c5cb2fd0
Greata <greata.greleven11@gmail.com>
2024-07-19 11:45:48 +0530

    Comments Added to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"; // Importing render and screen utilities from testing-library
+import App from "./App"; // Importing the App component to be tested
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  // Defining a test case for checking if the "learn react" link is rendered
+  render(<App />); // Rendering the App component
+  const linkElement = screen.getByText(/learn react/i); // Searching for an element with text matching "learn react"
+  expect(linkElement).toBeInTheDocument(); // Asserting that the found element is present in the document
 });
