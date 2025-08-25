38965210de883627014bebcf26d38c7dfe69866e
SaiKiran <s.racherla@crea2solutions.com>
2025-08-25 10:25:20 +0530

    --

--- src/Test/MapLegend.jsx
+++ src/Test/MapLegend.jsx
@@ -219,7 +219,7 @@ const MapLegend = ({ tiff, breadcrumbData, layerType, apiUrl, mapWidth, showHead
                             color: theme.palette.mode === "dark" ? theme.palette.text.secondary : "#111",
                           }}
                         >
-                          {item.population_value!== null ? calcpop(item.population_value) : "N/A"}
+                          {item.population_value ? calcpop(item.population_value) : "N/A"}
                         </Typography>
                       </Box>
                       <Box
@@ -309,7 +309,7 @@ const MapLegend = ({ tiff, breadcrumbData, layerType, apiUrl, mapWidth, showHead
                             color: theme.palette.mode === "dark" ? theme.palette.text.secondary : "#111",
                           }}
                         >
-                          {item.commodity_value !== null ? calcarea(item.commodity_value) : "N/A"}
+                          {item.commodity_value ? calcarea(item.commodity_value) : "N/A"}
                         </Typography>
                       </Box>
                     </Box>
